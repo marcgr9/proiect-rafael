eea836dadf21c3e12217dd338453a9e37b3282d7
Marc Gruita <marc@gruita.ro>
2022-11-13 20:22:22 +0200

    FEATURE Category combobox

--- src/App.tsx
+++ src/App.tsx
@@ -3,51 +3,82 @@ import { useEffect, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import ProductsGrid from './components/products-grid';
 import Searchbar from './components/search-bar';
-import Product from './model/product';
+import Product, { Category } from './model/product';
 import './utils/utils'
+import Combobox from 'react-widgets/Combobox';
+import "react-widgets/styles.css";
 
 import "./index.css"
 
 export default function App() {
-    const allProducts = [
+    const allProducts: Product[] = [
         {
             title: "ma ta",
             price: 1,
             picturePath: "./img/poza.jpg",
+            category: Category.BERE,
         },
         {
             title: "rafael",
             price: 1,
             picturePath: "./img/poza.jpg",
+            category: Category.BERE,
         },
         {
             title: "cacat",
             price: 1,
             picturePath: "./img/poza.jpg",
+            category: Category.WHISKEY,
+        },
+        {
+            title: "cacat bere",
+            price: 1,
+            picturePath: "./img/poza.jpg",
+            category: Category.BERE,
         },
         {
             title: "pula",
+            price: 1,
+            picturePath: "./img/poza.jpg",
+            category: undefined,
+        },
+        {
+            title: "pula fara categorie",
             price: 1,
             picturePath: "./img/poza.jpg",
         },
     ]
     
     const [products, setProducts] = useState<Product[]>(allProducts)
+    const [category, setCategory] = useState<Category>()
+    const [terms, setTerms] = useState<string[]>([])
+
+    useEffect(() => {
+        setProducts(
+            [...allProducts.filter(product =>
+                    (!terms.length || product.title.includesAny(terms)) &&
+                    (!category || !product.category || product.category == category)
+                )
+            ]
+        )
+    }, [category, terms])
 
     return (
         <div className="content-container">
             <Searchbar
-                onSearch={(terms: string[], raw: string) => {
-                    setProducts(
-                        terms.length ?
-                            [...allProducts.filter(product =>
-                                    product.title.includesAny(terms)
-                                )
-                            ]
-                        : [...allProducts]
-                    )
-                }}
+                onSearch={(terms: string[], raw: string) => setTerms(terms)}
             />
+            <div className="cacat-de-combobox-container">
+                <Combobox
+                    hideCaret
+                    data={Object.values(Category)}
+                    placeholder="Select a category"
+                    onSelect={(category) => {
+                        setCategory(category as Category)
+                    }}
+                    onChange={value => !value && setCategory(undefined)}
+                />
+            </div>
             <ProductsGrid
                 products={products}
             />
--- src/model/product.ts
+++ src/model/product.ts
@@ -2,4 +2,10 @@ export default interface Product {
     title: string,
     price: number,
     picturePath: string,
+    category?: Category,
+}
+
+export enum Category {
+    BERE = "bere",
+    WHISKEY = "whiskey",
 }
